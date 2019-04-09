6f8bb4b050490c98096cc754b3d770cfcd058d3c
Al Hove <hoveax16@juniata.edu>
2019-04-09 17:18:08 -0400

    Work on Task 2 & 3 with comments

---
+++ OMP_Project/OMP_ID.cpp
@@ -0,0 +1,44 @@
+/* Alyssa Hove and Katheryn Weeden
+ Task 3: A small college wants to know how many id numbers exist that meet these criteria:
+
+1. 6 digits 9^6
+2. first digit is non-zero : 8 * (9^5)
+3. two consecutive digits may not be the same: 
+4. sum of the digits may not be 7, 11 or 13
+*/
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <omp.h>
+#include <cstdlib>
+#include <windows.h>
+using namespace std;
+
+void main()
+{
+	int threadID;
+	int start;
+	int total;
+
+
+	start = GetTickCount();
+#pragma omp parallel sections private(k)
+	{  //thread 1 6 digits
+		
+		}
+#pragma omp section
+		// thread 2 first digit is not 0
+		{
+		}
+#pragma omp section
+		// thread 3 two consecutive 
+		{
+		}
+#pragma omp section
+		// thread 4 sum of digits 
+		{
+		}
+	}
+	cout << "Program ran in " << GetTickCount() - start << " TickCounts" << endl;
+}
--- OMP_Project/OMP_Project.cpp
+++ OMP_Project/OMP_Project.cpp
@@ -10,40 +10,36 @@
 #include <windows.h>
 using namespace std;
 
-void main()
-{
-	int threadID;
+void main(){
 	int totalThreads;
 	int maxThreads = 7;
+	int m;
+	int n;
+	int h;
 	int start;
-	/* Open MP pragma specifies that the following block is
-	  going to be parallel and the threadID variable is
-	  private in this openmp block.  Default num_threads=2
-	  */
-	cout << "Input number of threads: ";
-	cin >> maxThreads;
 
+	printf("\Please Input Value m\n");
+	cin >> m;
+	printf("\Please Input Value n\n");
+	cin >> n;
+	while (n<m) {
+		printf("\Sorry please input a value that is greater than m\n");
+		cin >> n;
+	}
+	printf("Please Input Value h");
+	cin >> h;
+    
 	start = GetTickCount();
-#pragma omp parallel num_threads (maxThreads) private(threadID)
+#pragma omp parallel num_threads (maxThreads) 
 	{
+		
+#pragma omp parallel
 
-		threadID = omp_get_thread_num();
-#pragma omp critical
-		cout << " \nHello World is from thread " << threadID << endl;
-		//printf("\nHello World is from thread %d\n", (int)threadID);
-		//master thread has threadID = 0
-		if (threadID == 0) {
-			//#pragma omp barrier  //if want to do this last
-			//#pragma omp master  //alternative to above if
 
 #pragma omp critical
-			cout << "\nMaster thread being called" << endl;
-			//printf("\nMaster thread being called\n");
-			totalThreads = omp_get_num_threads();
+			
+			
 #pragma omp critical
-			cout << "Total number of threads are " << totalThreads << endl;
-			//printf("Total number of threads are %d\n", totalThreads);
+			
 		}
 	}
-	cout << "Program ran in " << GetTickCount() - start << " TickCounts" << endl;
-}
