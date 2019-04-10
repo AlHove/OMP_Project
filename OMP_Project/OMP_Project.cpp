69b11f559c9bf1d5ff51f9114b97d4a07988b372
Al Hove <hoveax16@juniata.edu>
2019-04-10 17:17:08 -0400

    Easier to edit and run with comments

--- OMP_Project/OMP_Project.cpp
+++ OMP_Project/OMP_Project.cpp
@@ -4,7 +4,9 @@
 
 #include <iostream>
 #include <string>
-#include <stdio.h>
+#include <stdio.h> // printf
+#include <stdlib.h>  // Rand
+#include <ctime> //time
 #include <omp.h>
 #include <cstdlib>
 #include <windows.h>
@@ -13,10 +15,9 @@ using namespace std;
 void main(){
 	int totalThreads;
 	int maxThreads = 7;
-	int m;
-	int n;
-	int h;
-	int start;
+	int m, n, h, start, x, y, b, t;
+
+	srand(static_cast <unsigned int> (time(0))); // initialize rand
 
 	printf("\Please Input Value m\n");
 	cin >> m;
@@ -26,20 +27,22 @@ void main(){
 		printf("\Sorry please input a value that is greater than m\n");
 		cin >> n;
 	}
-	printf("Please Input Value h");
+	printf("Please Input Value for the height of your rectangle, h");
 	cin >> h;
     
 	start = GetTickCount();
+	//rand() % n + m;
+	// (b/t) * (h*(n-m))
+
 #pragma omp parallel num_threads (maxThreads) 
 	{
+		
 		
 #pragma omp parallel
 
 
 #pragma omp critical
 			
-			
-#pragma omp critical
 			
 		}
 	}
